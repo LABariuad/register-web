65c74d4539b78d78dc2bb1264a8d910bb0298fec
LABariuad <arwen.bariuad@gmail.com>
2024-03-05 16:04:23 +0800

    1st commit

---
+++ Desktop/Register-web-1/register.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+console.error("ERROR HAPPENED!!!");
+console.warn("WARNINNGGG!!!");
+
+// variables
+// string = "worddsss", "wowowo". "123"
+// number = 123, 434e, 2398.12
+// boolean = true, false
+// object = ["apple", "banana"], [`,2,3,4]
+// ["apple".3, true]
+
+//typeof
+// console.log(typeof(["apple", "banana"]));
+
+// Declaration of variables
+let name = "Lei"; // string
+lastname = "bharrie"; // string
+let age = 10; // number
+let isProf = true; // boolean
+hobbies = ("Watch movies"), // object
+run = function () {
+    console.log("I'm running");
+};
+console.log(name, age, isProf, hobbies, run);
+console.log(typeof run)
+console.table(hobbies);
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
